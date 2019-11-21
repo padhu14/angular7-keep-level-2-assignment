87053dd2ddbed6024f07009c9e04a598b7e1c068
Ubuntu <ubuntu@ip-172-31-39-188.ec2.internal>
2019-11-21 11:30:15 +0000

    completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,56 @@
 import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule, MatButtonModule, MatCardModule, MatExpansionModule, MatInputModule, MatFormFieldModule } from '@angular/material';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { CanActivateRouteGuard } from './can-activate-route.guard';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import { NotesService } from './services/notes.service';
+import { AuthenticationService } from './services/authentication.service';
+import { RouterService } from './services/router.service';
 
+const appRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [CanActivateRouteGuard],
+  }
+];
 @NgModule({
-  declarations: [ ],
-  imports: [ ],
-  providers: [ ],
-  bootstrap: [ ]
+  declarations: [ AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    DashboardComponent ],
+  imports: [ BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    RouterModule.forRoot(appRoutes) ],
+  providers: [ NotesService,
+    AuthenticationService,
+    RouterService,
+    CanActivateRouteGuard ],
+  bootstrap: [ AppComponent ]
 })
-
 export class AppModule { }
--- src/app/services/notes.service.ts
+++ src/app/services/notes.service.ts
@@ -1,20 +1,28 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Note } from '../note';
+import { AuthenticationService } from './authentication.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable()
 export class NotesService {
-
-  constructor() {
-
-  }
-
+  bearerToken: string;
+  token: any;
+  constructor(private httpClient: HttpClient,
+    private authService: AuthenticationService) {
+    }
   getNotes(): Observable<Array<Note>> {
-
+    this.token = this.authService.getBearerToken();
+    return this.httpClient.get<Array<Note>>('http://localhost:3000/api/v1/notes', {
+      headers: new HttpHeaders()
+      .set('Authorization', `Bearer ${this.token}`)
+    });
   }
-
   addNote(note: Note): Observable<Note> {
-    
+    return this.httpClient.post<Note>('http://localhost:3000/api/v1/notes', note, {
+      headers: new HttpHeaders()
+      .set('Authorization', `Bearer ${this.token}`)
+    });
   }
 
 }
